7a29c3a095b4243087bdf03437460ebc80950810
bitdriftlog <bitdriftlog@gmail.com>
2025-10-03 03:31:43 -0400

    implemet hero button video preview

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import Hero from "./components/Hero.jsx";
 
 const App = () => {
     return (
-        <main>
-            <h1 className="text-3xl text-orange-400 font-bold">
-                Welcome to Awwwords
-            </h1>
+        <main className="relative min-h-screen w-screen overflow-x-hidden">
+            <Hero />
+            <section className="z-0 min-h-screen bg-blue-100"></section>
         </main>
     )
 }
