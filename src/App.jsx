e0e9de0975319264dc15cf06a9f6931d639d31e3
bitdriftlog <bitdriftlog@gmail.com>
2025-10-03 03:53:32 -0400

    implemet about, animated title, features, navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Hero from "./components/Hero.jsx";
+import About from "./components/About.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Features from "./components/Features.jsx";
 
 const App = () => {
     return (
         <main className="relative min-h-screen w-screen overflow-x-hidden">
+            <Navbar />
             <Hero />
-            <section className="z-0 min-h-screen bg-blue-100"></section>
+            <About />
+            <Features />
         </main>
     )
 }
