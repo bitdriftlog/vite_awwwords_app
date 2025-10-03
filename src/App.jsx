fbe2b37f39c71147b599cce28811186a8be762e4
bitdriftlog <bitdriftlog@gmail.com>
2025-10-03 01:09:55 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+const App = () => {
+    return (
+        <main>
+            <h1 className="text-3xl text-orange-400 font-bold">
+                Welcome to Awwwords
+            </h1>
+        </main>
+    )
+}
+export default App
